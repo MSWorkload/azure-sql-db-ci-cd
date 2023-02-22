6498e0c2886431a286a565c20f962bdef72fb2e1
Ankit Desai <ankitdesai@microsoft.com>
2023-02-22 13:33:58 -0500

    update to the environment variable

--- db-deploy/Program.cs
+++ db-deploy/Program.cs
@@ -10,9 +10,10 @@ namespace AzureSQLDevelopers.Database.Deploy
         {
             DotNetEnv.Env.Load();
 
-            var connectionString = "Server=tcp:dacpacsvr.database.windows.net,1433;Database=demoDbUp;Persist Security Info=False;User ID=github_action_user;Password=S0meVery_Very+Str0ngPazzworD!;Encrypt=true;TrustServerCertificate=False;Connection Timeout=30;";
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+            
 
-                //Environment.GetEnvironmentVariable("ConnectionString");
+            
 
             var upgrader = DeployChanges.To
                 .SqlDatabase(connectionString)
